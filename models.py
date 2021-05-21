71e5d4bbeadd00f862576fce5e0349030e2ad537
Christopher_Madrid <ChrisMadrid173@gmail.com>
2021-05-21 13:29:39 -0500

    Cleaned up unused stuff from refernce code

--- models.py
+++ models.py
@@ -1,28 +1,7 @@
-"""
-Adapted from https://github.com/SaoYan/DnCNN-PyTorch/blob/master/models.py
-"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
 from torch.autograd import Variable
-
-class DnCNN(nn.Module):
-    def __init__(self, channels=1, num_of_layers=9, kernel_size=3, features=100):
-        super(DnCNN, self).__init__()
-        padding = int((kernel_size-1)/2)
-        layers = []
-        layers.append(nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
-        layers.append(nn.ReLU(inplace=True))
-        for _ in range(num_of_layers-2):
-            layers.append(nn.Conv2d(in_channels=features, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
-            layers.append(nn.ReLU(inplace=True))
-
-        layers.append(nn.Conv2d(in_channels=features, out_channels=channels, kernel_size=kernel_size, padding=padding, bias=False))
-        self.dncnn = nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = self.dncnn(x)
-        return out
 
 class DNN(nn.Module):
     def __init__(self, n_var=10, n_layers=1, n_nodes=20, n_outputs=2, drop_out_p=0.3):
@@ -38,50 +17,12 @@ class DNN(nn.Module):
         layers.append(nn.Dropout(p=drop_out_p))
         layers.append(nn.Linear(n_nodes, n_outputs))
 
+
+
         self.dnn = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.dnn(x)
-
-class PatchLoss(nn.Module):
-    def __initII(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
-        super(PatchLoss, self).__init__(size_average, reduce, reduction)
-
-    def forward(self, output, target, patch_size=50):
-        avg_loss = 0
-        for i in range(len(output)):
-            # split output and target images into patches
-            output_patches = output[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
-            target_patches = target[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
-            max_patch_loss = 0
-            # calculate loss for each patch of the image
-            for j in range(list(output_patches.size())[0]):
-                for k in range(list(output_patches.size())[1]):
-                    max_patch_loss = max(max_patch_loss, f.l1_loss(output_patches[j][k], target_patches[j][k]))
-            avg_loss+=max_patch_loss
-        avg_loss/=len(output)
-        #print(avg_loss)
-        return avg_loss;
-
-class WeightedPatchLoss(nn.Module):
-    def __initII(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
-        super(PatchLoss, self).__init__(size_average, reduce, reduction)
-
-    def forward(self, output, target, patch_size):
-        avg_loss = 0
-        for i in range(len(output)):
-            # split output and target images into patches
-            output_patches = output[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
-            target_patches = target[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
-            weighted_loss = 0
-            # calculate loss for each patch of the image
-            for j in range(list(output_patches.size())[0]):
-                for k in range(list(output_patches.size())[1]):
-                    weighted_loss += f.l1_loss(output_patches[j][k],target_patches[j][k]) * torch.mean(target_patches[j][k])
-            avg_loss+=weighted_loss/torch.mean(target[i])
-        avg_loss/=len(output)
-        return avg_loss;
-
 
 if __name__=="__main__":
     # Test CrossEntropyLoss function
@@ -107,3 +48,4 @@ if __name__=="__main__":
     target = torch.empty(5, dtype=torch.long).random_(2)
     output = loss(out, target)
     output.backward()
+
