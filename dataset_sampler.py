eecae413c38ae1fda536e521a10a300f36ff8981
Keane-Tan <chin.lung.t93@gmail.com>
2023-02-06 13:55:45 -0600

    now can use a specific jet category, not just all signal jets, for reference distribution (useful if more than one SVJ category is used)

--- dataset_sampler.py
+++ dataset_sampler.py
@@ -1,25 +1,46 @@
 import numpy as np
 import mplhep as hep
 
-def getCondition(inputFileNames,key,inputFileIndices):
-    conditions = np.zeros(len(inputFileIndices),dtype=bool)
-    for i in range(len(inputFileNames)):
-        fileName = inputFileNames[i]
-        if key in fileName:
-            conditions = conditions | (inputFileIndices == i)
-    return conditions
+labelDict = {
+    0:"QCD",
+    1:"TTJets",
+    2:"Dark",
+}
 
-def countJets(inputFileNames,key,inputFileIndices):
-    print("{}: {}".format(key,np.sum(getCondition(inputFileNames,key,inputFileIndices))) )
+def getCondition(label,key):
+    return label == key
 
-# we are matching the pT distributions to the SVJPt distribution
-def getReferencePtHist(inputFileNames,inputFileIndices,pT,weight):
+def countJets(labelDict, label, key):
+    print("Jet {}: {}".format(labelDict[key],np.sum(getCondition(label,key))) )
+
+def getReferencePtHist(label,pT,weight,randomGenerator):
     pTBin = np.arange(0,3000,50)
-    SVJCond = getCondition(inputFileNames,"SVJ",inputFileIndices)
-    referencePtHist, bins = np.histogram(pT[SVJCond],pTBin)
-    return referencePtHist,pTBin
+    useLab = 2
+    fracToUse = 1.0
+    indices = np.where(getCondition(label,useLab))[0]
+    print("indices length:", len(indices))
+    useInd = randomGenerator.choice(indices,int(len(indices)*fracToUse),replace=False)
+    print("useInd length:",len(useInd))
+    referencePtHist, bins = np.histogram(np.take(pT,useInd,axis=0),pTBin)
+    return referencePtHist,pTBin,useInd
 
-def getJetIndices(referenceHist,pTBin,key,inputFileNames,inputFileIndices,pT,weight):
+def getSigJetIndices(referenceHist,pTBin,label,key,pT):
+    digitpT = np.digitize(pT,pTBin) - 1
+    jetCond = getCondition(label,key)
+    finalIndices = []
+    for j in range(len(referenceHist)):
+        nJ_j = referenceHist[j]
+        allJ_j_pos = np.where((digitpT == j) & (jetCond))[0]
+        if (nJ_j == 0) or (len(allJ_j_pos) == 0):
+            continue
+        if len(allJ_j_pos) > nJ_j:
+            finalIndices += list(randomGenerator.choice(allJ_j_pos,nJ_j,replace=False))
+        else:
+            finalIndices += list(randomGenerator.choice(allJ_j_pos,nJ_j,replace=True))
+    return finalIndices
+
+# this not only samples the background jets in a way that match some reference histogram, it also makes sure that the proportion of jets in each pT bin matches the actual proportion of jets from each subsample in that bin
+def getBkgJetIndices(referenceHist,pTBin,key,inputFileNames,inputFileIndices,pT,weight,randomGenerator):
     weightedHistList = []
     histFileConList = []
     finalIndices = []
@@ -45,48 +66,50 @@ def getJetIndices(referenceHist,pTBin,key,inputFileNames,inputFileIndices,pT,wei
             if (nJ_j == 0) or (len(allJ_j_pos) == 0):
                 continue
             if len(allJ_j_pos) > nJ_j:
-                finalIndices += list(np.random.choice(allJ_j_pos,nJ_j,replace=False))
+                finalIndices += list(randomGenerator.choice(allJ_j_pos,nJ_j,replace=False))
             else:
-                finalIndices += list(np.random.choice(allJ_j_pos,nJ_j,replace=True))
+                finalIndices += list(randomGenerator.choice(allJ_j_pos,nJ_j,replace=True))
     return finalIndices
 
 inFolder = "processedDataNPZ"
-inNPZFileName = "processedData_nc100_train"
+inNPZFileName = "processedData_validation"
 npzFile = np.load("{}/{}.npz".format(inFolder,inNPZFileName))
 inputPoints = npzFile["inputPoints"]
 inputFeatures = npzFile["inputFeatures"]
-inputJetFeatures = npzFile["inputJetFeatures"]
+masks = npzFile["masks"]
+inputFileIndices = npzFile["inputFileIndices"]
 signal = npzFile["signal"]
 pT = npzFile["pT"]
 weight = npzFile["weight"]
 mMed = npzFile["mMed"]
 mDark = npzFile["mDark"]
 rinv = npzFile["rinv"]
-alpha = npzFile["alpha"]
+label = np.where(signal==1)[1]
 inputFileNames = npzFile["inputFileNames"]
-inputFileIndices = npzFile["inputFileIndices"]
 inputFeaturesVarName = npzFile["inputFeaturesVarName"]
 print("Before weighting:")
 print("Total jets used in training",len(inputFileIndices))
-countJets(inputFileNames,"QCD",inputFileIndices)
-countJets(inputFileNames,"TTJets",inputFileIndices)
-countJets(inputFileNames,"SVJ",inputFileIndices)
+for key in np.unique(label):
+    countJets(labelDict, label, key)
+
+randomGenerator = np.random.default_rng(888)
 # reference histogram is the sum of all SVJ signals' pT
-referenceHist,pTBin = getReferencePtHist(inputFileNames,inputFileIndices,pT,weight)
-QCDFinalIndices = getJetIndices(referenceHist,pTBin,"QCD",inputFileNames,inputFileIndices,pT,weight)
-TTJetsFinalIndices = getJetIndices(referenceHist,pTBin,"TTJets",inputFileNames,inputFileIndices,pT,weight)
-SVJCond = getCondition(inputFileNames,"SVJ",inputFileIndices)
-SVJFinalIndices = np.where(SVJCond)[0]
-finalIndices = QCDFinalIndices + TTJetsFinalIndices + list(SVJFinalIndices)
-print("QCD jets used in training",len(QCDFinalIndices))
-print("TTJets jets used in training",len(TTJetsFinalIndices))
-print("SVJ jets used in training",len(SVJFinalIndices))
-print("Total jets used in training",len(finalIndices))
+referenceHist,pTBin,referenceIndices = getReferencePtHist(label,pT,weight,randomGenerator)
+QCDFinalIndices = getBkgJetIndices(referenceHist,pTBin,"QCD",inputFileNames,inputFileIndices,pT,weight,randomGenerator)
+TTJetsFinalIndices = getBkgJetIndices(referenceHist,pTBin,"TTJets",inputFileNames,inputFileIndices,pT,weight,randomGenerator)
+#mixDarkIndices = getSigJetIndices(referenceHist,pTBin,label,3,pT)
+finalIndices = QCDFinalIndices + TTJetsFinalIndices + list(referenceIndices) # + mixDarkIndices 
+
+useLabel = np.take(label,finalIndices,axis=0)
+print("Number of jets by jet category")
+print(np.unique(useLabel,return_counts=True))
+
+#raise Exception("Arretez")
 
 dictOut = {
     "inputPoints":np.take(inputPoints,finalIndices,axis=0),
     "inputFeatures":np.take(inputFeatures,finalIndices,axis=0),
-    "inputJetFeatures":np.take(inputJetFeatures,finalIndices,axis=0),
+    "masks":np.take(masks,finalIndices,axis=0),
     "inputFileIndices":np.take(inputFileIndices,finalIndices,axis=0),
     "signal":np.take(signal,finalIndices,axis=0),
     "pT":np.take(pT,finalIndices,axis=0),
@@ -94,14 +117,9 @@ dictOut = {
     "mMed":np.take(mMed,finalIndices,axis=0),
     "mDark":np.take(mDark,finalIndices,axis=0),
     "rinv":np.take(rinv,finalIndices,axis=0),
-    "alpha":np.take(alpha,finalIndices,axis=0),
     "inputFileNames":inputFileNames,
     "inputFeaturesVarName":inputFeaturesVarName
 }
-
-countJets(inputFileNames,"QCD",dictOut["inputFileIndices"])
-countJets(inputFileNames,"TTJets",dictOut["inputFileIndices"])
-countJets(inputFileNames,"SVJ",dictOut["inputFileIndices"])
 
 #print("pT",len(dictOut['pT']))
 #print("weight",len(dictOut['weight']))
