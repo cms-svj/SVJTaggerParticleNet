917511e95fbfdff9b8bb41d03e87b6c7a711fee1
Keane-Tan <chin.lung.t93@gmail.com>
2023-04-14 13:48:07 -0500

    Extend wc time limit to 1 day

--- wcSBatch.sh
+++ wcSBatch.sh
@@ -4,7 +4,9 @@
 #SBATCH --constraint=v100
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_gce
+#SBATCH --qos=regular
 #SBATCH --gres=gpu:1
+#SBATCH --time=1-00:00:00
 
 pwd
 export dirname="/work1/cms_svj/keane/particleNet/SVJTaggerParticleNet/"
